c1504eea8a3a98f859fe21022fdc0e9ab12fae4e
Niklas Haas <git@haasn.xyz>
2016-09-30 10:34:37 +0200

    Implement some xapian integration

--- HsBooru.hs
+++ HsBooru.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE TemplateHaskell, RecordWildCards, PatternSynonyms, ViewPatterns #-}
+{-# LANGUAGE TemplateHaskell, RecordWildCards, PatternSynonyms, ViewPatterns,
+             LambdaCase #-}
 
 import Control.Applicative ((<|>))
 import Control.Monad
@@ -8,18 +9,25 @@ import Control.Monad.IO.Class
 
 import Data.Time
 import Data.SafeCopy
-import Data.Char (isNumber)
+import Data.Serialize.Get (runGet)
+import Data.Serialize.Put (runPut)
+import Data.Char (isNumber, toLower)
 import Data.List (find, isPrefixOf)
-import Data.Maybe (listToMaybe)
+import Data.Maybe (maybeToList)
 
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.UTF8 as UTF8
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
 
 import Network.Curl ( CurlCode(..), CurlOption(..)
                     , CurlResponse_(..), curlGetResponse_ )
 
+import Search.Xapian
 import System.Directory (doesFileExist)
 import System.FilePath.Posix ((</>), takeFileName)
+
+import qualified Search.Xapian.Query.Combinators as Q
 
 import Text.Read (readMaybe)
 import Text.HTML.Scalpel hiding (curlOpts)
@@ -27,11 +35,12 @@ import Text.HTML.Scalpel hiding (curlOpts)
 -- ^ Global configuration
 
 -- Directories
-dbDir     = "/z/booru/"
-imageDir  = dbDir </> "images"
-xapianDir = dbDir </> "xapian"
+dbDir       = "/z/booru/"
+imageDir    = dbDir </> "images"
+xapianDir   = dbDir </> "xapian"
 
-curlOpts  = [CurlFollowLocation True]
+-- Used when downloading images
+curlOpts    = [CurlFollowLocation True]
 
 -- ^ Types and instances
 
@@ -45,7 +54,9 @@ deriveSafeCopy 0 'base ''FileInfo
 -- These types are only used within the scraper, and don't need to be stored
 
 data Post = Post
-    { rating   :: !Rating
+    { siteID   :: !Int
+    , booru    :: !String
+    , rating   :: !Rating
     , uploader :: !String
     , score    :: !Int
     , source   :: !(Maybe String)
@@ -67,7 +78,7 @@ data TagType = General | Character | Artist | Work
 data SiteScraper = SiteScraper
     { siteName    :: String
     , scrapeID    :: Int -> IO (Maybe Post)
-    , scrapeCount :: IO (Maybe Int)
+    , scrapeMax   :: IO (Maybe Int)
     }
 
 -- ^ Scraper definitions
@@ -107,7 +118,7 @@ gelbooru :: SiteScraper
 gelbooru = SiteScraper{..}
     where siteName = "gelbooru"
 
-          scrapeCount = scrapeURL rssUrl $ do
+          scrapeMax = scrapeURL rssUrl $ do
                 newestID <- fmap extractID . text $ "item" // "link"
                 case newestID of
                     Just id -> return id
@@ -118,6 +129,9 @@ gelbooru = SiteScraper{..}
           -- so we're stuck with manually parsing the website if we want
           -- all of the information..
           scrapeID postID = scrapeURL (postUrl ++ show postID) $ do
+                let siteID  = postID
+                    booru = siteName
+
                 -- Extract the stats by parsing the list on the left
                 let stats = "div" @: ["id" @= "stats"] // "li"
                 uploader <- text $ stats // "a" -- luckily the first link
@@ -131,18 +145,21 @@ gelbooru = SiteScraper{..}
 
                 let tag = do Just tc <- getTagType <$> attr "class" "li"
                              [_, tn] <- texts $ "li" // "a"
-                             return Tag { tagType = tc, tagName = tn }
+                             return Tag { tagType = tc, tagName = sanitize tn }
 
                     getTagType "tag-type-general"   = Just General
                     getTagType "tag-type-character" = Just Character
                     getTagType "tag-type-artist"    = Just Artist
                     getTagType "tag-type-copyright" = Just Work
+                    getTagType _ = Nothing
+
+                    sanitize = map (\case ' ' -> '_'; c -> c)
 
                 tags <- chroots ("ul" @: ["id" @= "tag-sidebar"] // "li") tag
 
                 return Post{..}
 
-          rssUrl = "http://gelbooru.com/index.php?page=cooliris"
+          rssUrl  = "http://gelbooru.com/index.php?page=cooliris"
           postUrl = "http://gelbooru.com/index.php?page=post&s=view&id="
 
 -- ^ Image downloading and storing
@@ -168,6 +185,113 @@ download url path = do
             -> liftIO $ LBS.writeFile path img
 
         CurlResponse { respCurlCode = err }
-            -> throwE $ show err
+            -> throwE $ "Error downloading " ++ url ++ ": " ++ show err
 
--- TODO: thumbnail generation (?), xapian integration, frontend
+
+-- ^ Xapian database integration
+
+addTag :: Document -> String -> XapianM ()
+addTag doc str = addTerm (UTF8.fromString $ sanitize str) doc
+    where sanitize (p:str) = p : map toLower str
+
+showLower :: Show a => a -> String
+showLower = map toLower . show
+
+xapianStore :: WritableDatabase db => db -> Post -> FileInfo -> XapianM ()
+xapianStore db Post{..} fi = do
+    doc <- emptyDocument
+    setData (runPut $ safePut fi) doc
+
+    addTag doc $ siteIDPrefix   : show siteID
+    addTag doc $ booruPrefix    : booru
+    addTag doc $ ratingPrefix   : showLower rating
+    addTag doc $ uploaderPrefix : uploader
+    addTag doc $ scorePrefix    : show score
+    addTag doc $ urlPrefix      : fileURL
+    forM_ source $ \s       -> addTag doc (sourcePrefix : s)
+    forM_ tags   $ \Tag{..} -> addTag doc (tagPrefix tagType : tagName)
+
+    addDocument db doc
+    commit db
+
+xapianRW :: ExceptT String IO ReadWriteDB
+xapianRW = requireRight $ openReadWrite CreateOrOpen xapianDir
+
+parseQuery :: String -> Query
+parseQuery str = Q.queryAll [ parseTerm t | t <- words str ]
+
+xapianRO :: ExceptT String IO ReadOnlyDB
+xapianRO = requireRight $ openReadOnly xapianDir
+
+runXM :: XapianM a -> ExceptT String IO a
+runXM = liftIO . runXapian
+
+xapianSearch :: ReadableDatabase db => db -> Query -> QueryRange -> XapianM [Document]
+xapianSearch db query range = snd <$> search db query range
+
+allResults :: QueryRange
+allResults = QueryRange 0 maxBound
+
+-- ^ Front-end routines for the primitives above
+
+processID :: WritableDatabase db => db -> SiteScraper -> Int -> ExceptT String IO Post
+processID db SiteScraper{..} id = do
+    post <- requireJust "Scraper failed to scrape post" $ scrapeID id
+    file <- fetchImage (fileURL post)
+    runXM $ xapianStore db post file
+    return post
+
+listFiles :: ReadableDatabase db => db -> Query -> ExceptT String IO [FileInfo]
+listFiles db q = do
+    ds <- runXM $ xapianSearch db q allResults >>= mapM getData
+    forM ds $ ExceptT . pure . runGet safeGet
+
+-- Misc helpers
+
+requireJust :: String -> IO (Maybe a) -> ExceptT String IO a
+requireJust err = maybe (throwE err) pure <=< liftIO
+
+requireRight :: Show e => IO (Either e a) -> ExceptT String IO a
+requireRight = either (throwE . show) pure <=< liftIO
+
+-- Prefix mapping
+
+tagPrefix :: TagType -> Char
+tagPrefix General   = 'G'
+tagPrefix Character = 'C'
+tagPrefix Artist    = 'A'
+tagPrefix Work      = 'W'
+
+siteIDPrefix   = 'I'
+booruPrefix    = 'B'
+ratingPrefix   = 'R'
+uploaderPrefix = 'U'
+scorePrefix    = 'S'
+sourcePrefix   = 'R'
+urlPrefix      = 'L'
+
+prefixFriendly :: [(Char, [String])]
+prefixFriendly =
+    [ (siteIDPrefix,   ["id:"])
+    , (booruPrefix,    ["booru:", "scraper:", "site:"])
+    , (ratingPrefix,   ["rating:"])
+    , (uploaderPrefix, ["uploader:", "creator:"])
+    , (scorePrefix,    ["score:"])
+    , (sourcePrefix,   ["source:"])
+    , (urlPrefix,      ["url:"])
+
+    , (tagPrefix Character, ["character:", "char:"])
+    , (tagPrefix Artist,    ["artist:", "author:"])
+    , (tagPrefix Work,      ["work:", "copyright:"])
+
+    -- This must be the last entry
+    , (tagPrefix General, [""])
+    ]
+
+parseTerm :: String -> String
+parseTerm str = head [ p:r | (p, ls) <- prefixFriendly
+                           , l <- ls
+                           , r <- maybeToList $ chomp l str
+                     ]
+
+-- TODO: thumbnail generation (?), frontend
