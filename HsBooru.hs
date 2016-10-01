7567236a203f4c17871ab6afe28f75bbaee2a139
Niklas Haas <git@haasn.xyz>
2016-10-01 14:51:14 +0200

    Major overhaul: Rework design and use the API instead
    Instead of fetching posts one-by-one and downloading their images
    alternatingly, just batch fetch all post information up-front and do the
    image downloading with a separate / dedicated tool (or mode of
    operation).
    
    This will both improve performance (since the old code did a request per
    post and then a request per image) and make the system more robust
    (because the API will never randomly fail on an individual-post basis,
    and because I can distinguish between known-but-failed-download posts,
    unknown posts and known-but-download-needed posts).
    
    Also, rework the way the xapian database is organized - use values
    instead of terms for per-post-unique data (like file IDs) and stuff like
    scores that we will want to sort on, and drop the ‘type’ from tags.
    
    (They are not really necessary, and more importantly, the API doesn't
    give it - so I'll just live with using bare terms. This also makes it
    easier for me to use the existing xapian front-end tools, because those
    are designed around bare terms as well)
    
    Finally, simplify the code a great deal by dropping some unnecessary
    stuff. Get rid of the SafeCopy baggage because it hurts interoperability
    with other stuff (e.g. python front-ends) and because terms/values are
    flexible enough to prevent me from ever wanting expand the document
    contents themselves either way.

--- HsBooru.hs
+++ HsBooru.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE TemplateHaskell, RecordWildCards, PatternSynonyms, ViewPatterns,
-             LambdaCase #-}
+             LambdaCase, TypeFamilies #-}
 
-import Control.Applicative ((<|>))
+import Prelude hiding (log)
+
+import Control.Applicative
 import Control.Monad
 import Control.Monad.Fail
 import Control.Monad.Trans.Except
 import Control.Monad.IO.Class
 
+import Data.Ix (inRange)
 import Data.Time
 import Data.SafeCopy
-import Data.Serialize.Get (runGet)
-import Data.Serialize.Put (runPut)
-import Data.Char (isNumber, toLower)
+import Data.Serialize
+import Data.Char
 import Data.List (find, isPrefixOf)
 import Data.Maybe (maybeToList)
 
@@ -31,55 +33,54 @@ import System.IO
 import qualified Search.Xapian.Query.Combinators as Q
 
 import Text.Read (readMaybe)
-import Text.HTML.Scalpel hiding (curlOpts)
+import Text.HTML.Scalpel hiding (curlOpts, scrape)
 
--- ^ Global configuration
+-- ^ Global configuration and constants
 
 -- Directories
 dbDir       = "/z/booru/"
 imageDir    = dbDir </> "images"
 xapianDir   = dbDir </> "xapian"
 
--- Used when downloading images
-curlOpts    = [CurlFollowLocation True]
+-- Term prefix mapping, for reusable tags
+booruPrefix    = "B"
+uploaderPrefix = "U"
+ratingPrefix   = "R"
+tagPrefix      = ""
+
+-- Because we're collecting post data separately from performing the actual
+-- downloading, we use tags to signal post state
+statePrefix    = "X"
+unprocessedTag = "todo"
+failedTag      = "failed"
+
+-- Value mapping, for per-document unique identification
+siteIDSlot   = 0
+scoreSlot    = 1
+fileNameSlot = 2
+sourceSlot   = 3
 
 -- ^ Types and instances
-
--- This is defined via SafeCopy so I can migrate to a newer version
--- in the future
-
-data FileInfo = FileInfo { fileName :: !T.Text } deriving Show
-
-deriveSafeCopy 0 'base ''FileInfo
-
--- These types are only used within the scraper, and don't need to be stored
 
 data Post = Post
     { siteID   :: !Int
     , booru    :: !String
     , rating   :: !Rating
-    , uploader :: !String
+    , uploader :: !Int
     , score    :: !Int
     , source   :: !(Maybe String)
     , fileURL  :: !String
-    , tags     :: ![Tag]
+    , fileName :: !String
+    , tags     :: ![String]
     } deriving Show
 
 data Rating = Safe | Questionable | Explicit
     deriving (Show, Read)
 
-data Tag = Tag
-    { tagType :: !TagType
-    , tagName :: !String
-    } deriving Show
-
-data TagType = General | Character | Artist | Work
-    deriving (Show, Read)
-
 data SiteScraper = SiteScraper
-    { siteName    :: String
-    , scrapeID    :: Int -> IO (Maybe Post)
-    , scrapeMax   :: IO (Maybe Int)
+    { siteName   :: String
+    , pageSize   :: !Int
+    , scrapePage :: Int -> IO (Maybe [Post])
     }
 
 -- ^ Scraper definitions
@@ -98,85 +99,58 @@ chomp (x:xs) (y:ys)
     | x == y    = chomp xs ys
     | otherwise = Nothing
 
--- More useful scraper primitives
-textMaybe :: Selectable s => s -> Scraper String (Maybe String)
-textMaybe s = Just <$> text s <|> pure Nothing
-
-textFirst :: Selectable s => (String -> Maybe a) -> s -> Scraper String a
-textFirst f s = texts s >>= go
-    where go [] = mzero
-          go ((f -> Just x):xs) = pure x
-          go (_:xs) = go xs
-
-textRead :: (Selectable s, Read a) => s -> Scraper String a
-textRead = maybe mzero pure . readMaybe <=< text
-
-(@^=) :: String -> String -> AttributePredicate
-k @^= v = match $ \k' v' -> k == k' && v `isPrefixOf` v'
+attrRead :: (Selectable s, Read a) => String -> s -> Scraper String a
+attrRead a = maybe mzero pure . readMaybe <=< attr a
 
 -- Gelbooru scraper
 gelbooru :: SiteScraper
 gelbooru = SiteScraper{..}
     where siteName = "gelbooru"
+          pageSize = 100
 
-          scrapeMax = scrapeURL rssUrl $ do
-                newestID <- fmap extractID . text $ "item" // "link"
-                case newestID of
-                    Just id -> return id
-                    Nothing -> mzero
+          scrapePage page = scrapeURL (apiURL ++ show page) $
+                chroots "post" scrapePost
 
-          -- NOTE: gelbooru has a programmable API which would make
-          -- most of this easier, but unfortunately it has some limitations
-          -- so we're stuck with manually parsing the website if we want
-          -- all of the information..
-          scrapeID postID = scrapeURL (postUrl ++ show postID) $ do
-                let siteID  = postID
+          scrapePost = do
+                let post  = "post"
                     booru = siteName
 
-                -- Extract the stats by parsing the list on the left
-                let stats = "div" @: ["id" @= "stats"] // "li"
-                uploader <- text $ stats // "a" -- luckily the first link
-                rating   <- textFirst getRating stats
-                score    <- textRead  $ stats // "span" @: ["id" @^= "psc"]
-                source   <- textMaybe $ stats // "a" @: ["rel" @= "nofllow"]
+                siteID   <- attrRead "id" post
+                fileURL  <- attr     "file_url"   post
+                uploader <- attrRead "creator_id" post
+                score    <- attrRead "score"      post
+                tags     <- attr "tags"   post <&> words
+                rating   <- attr "rating" post <&> \case "s" -> Safe
+                                                         "q" -> Questionable
+                                                         "e" -> Explicit
 
-                -- This is the easiest way to match the "Original image" link
-                fileURL <- attr "href" $ "div" // "li" // "a"
-                                       @: ["style" @= "font-weight: bold;"]
+                source   <- attr "source" post <&> \case ""  -> Nothing
+                                                         src -> Just src
 
-                let tag = do Just tc <- getTagType <$> attr "class" "li"
-                             [_, tn] <- texts $ "li" // "a"
-                             return Tag { tagType = tc, tagName = sanitize tn }
 
-                    getTagType "tag-type-general"   = Just General
-                    getTagType "tag-type-character" = Just Character
-                    getTagType "tag-type-artist"    = Just Artist
-                    getTagType "tag-type-copyright" = Just Work
-                    getTagType _ = Nothing
-
-                    sanitize = map (\case ' ' -> '_'; c -> c)
-
-                tags <- chroots ("ul" @: ["id" @= "tag-sidebar"] // "li") tag
+                -- Since we're working with direct links, we can extract the
+                -- "filename" from the URL as if it were a path. Kludgy, but
+                -- works..
+                let fileName = takeFileName fileURL
 
                 return Post{..}
 
-          rssUrl  = "http://gelbooru.com/index.php?page=cooliris"
-          postUrl = "http://gelbooru.com/index.php?page=post&s=view&id="
+          apiURL = "http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&pid="
 
 -- ^ Image downloading and storing
 
+{-
 fetchImage :: URL -> ExceptT String IO FileInfo
 fetchImage url = do
     -- Since we're working with direct links, we can extract the "filename"
     -- from the URL as if it were a path. Kludgy, but works..
     let fileName = takeFileName url
         filePath = imageDir </> fileName
-        fileInfo = FileInfo { fileName = T.pack fileName }
 
     e   <- liftIO $ doesFileExist filePath
     unless e $ download url filePath
 
-    return fileInfo
+    return FileInfo{..}
 
 download :: URL -> FilePath -> ExceptT String IO ()
 download url path = do
@@ -187,90 +161,76 @@ download url path = do
 
         CurlResponse { respCurlCode = err }
             -> throwE $ "Error downloading " ++ url ++ ": " ++ show err
-
+-}
 
 -- ^ Xapian database integration
 
-addTag :: Document -> String -> XapianM ()
-addTag doc str = addTerm (UTF8.fromString $ sanitize str) doc
-    where sanitize (p:str) = p : map toLower str
+addTag :: Document -> String -> String -> XapianM ()
+addTag doc prefix str = addTerm (UTF8.fromString sanitized) doc
+    where sanitized = prefix ++ map fix str
 
-showLower :: Show a => a -> String
-showLower = map toLower . show
+          fix ' ' = '_'
+          fix c   = toLower c
 
-xapianStore :: WritableDatabase db => db -> Post -> FileInfo -> XapianM ()
-xapianStore db Post{..} fi = do
+strVal :: Document -> ValueNumber -> String -> XapianM ()
+strVal doc val str = setValue val (UTF8.fromString sanitized) doc
+    where sanitized = map toLower str
+
+encVal :: Serialize a => Document -> ValueNumber -> a -> XapianM ()
+encVal doc val num = setValue val (encode num) doc
+
+xapianStore :: WritableDatabase db => db -> Post -> XapianM ()
+xapianStore db Post{..} = do
     doc <- emptyDocument
-    setData (runPut $ safePut fi) doc
+    addTag doc statePrefix unprocessedTag
+    setData (encode fileURL) doc
 
-    addTag doc $ siteIDPrefix   : show siteID
-    addTag doc $ booruPrefix    : booru
-    addTag doc $ ratingPrefix   : showLower rating
-    addTag doc $ uploaderPrefix : uploader
-    addTag doc $ scorePrefix    : show score
-    addTag doc $ urlPrefix      : fileURL
-    forM_ source $ \s       -> addTag doc (sourcePrefix : s)
-    forM_ tags   $ \Tag{..} -> addTag doc (tagPrefix tagType : tagName)
+    encVal doc siteIDSlot siteID
+    encVal doc scoreSlot score
+    strVal doc fileNameSlot fileName
+    forM_ source $ strVal doc sourceSlot
 
-    addDocument db doc
-    commit db
+    addTag doc booruPrefix    $ booru
+    addTag doc uploaderPrefix $ show uploader
+    addTag doc ratingPrefix   $ show rating
+    forM_ tags $ addTag doc tagPrefix
 
-xapianRW :: ExceptT String IO ReadWriteDB
-xapianRW = requireRight $ openReadWrite CreateOrOpen xapianDir
+    void $ addDocument db doc
 
-parseQuery :: String -> Query
-parseQuery str = Q.queryAll [ parseTerm t | t <- words str ]
-
-xapianRO :: ExceptT String IO ReadOnlyDB
-xapianRO = requireRight $ openReadOnly xapianDir
+xapianDB :: ExceptT String IO ReadWriteDB
+xapianDB = requireRight $ openReadWrite CreateOrOpen xapianDir
 
 runXM :: XapianM a -> ExceptT String IO a
-runXM = liftIO . runXapian
-
-xapianSearch :: ReadableDatabase db => db -> Query -> QueryRange -> XapianM [Document]
-xapianSearch db query range = snd <$> search db query range
-
-allResults :: QueryRange
-allResults = QueryRange 0 maxBound
+runXM = io . runXapian
 
 -- ^ Front-end routines for the primitives above
 
-processID :: WritableDatabase db => db -> SiteScraper -> Int -> ExceptT String IO Post
-processID db SiteScraper{..} id = do
-    post <- requireJust "Scraper failed to scrape post" $ scrapeID id
-    file <- fetchImage (fileURL post)
-    runXM $ xapianStore db post file
-    return post
+scrapeSite :: SiteScraper -> Int -> Int -> Int -> ExceptT String IO ()
+scrapeSite SiteScraper{..} offset minID maxID = xapianDB >>= go startPage
+    where startPage = offset `div` pageSize
 
-listFiles :: ReadableDatabase db => db -> Query -> ExceptT String IO [FileInfo]
-listFiles db q = do
-    ds <- runXM $ xapianSearch db q allResults >>= mapM getData
-    forM ds $ ExceptT . pure . runGet safeGet
+          go :: Int -> ReadWriteDB -> ExceptT String IO ()
+          go n db = do
+            io.log siteName $ "Scraping page " ++ show n
+            res <- io $ scrapePage n
 
-scrapeSiteFrom :: SiteScraper -> Int -> IO ()
-scrapeSiteFrom site@SiteScraper{..} from = do
-    maxID <- scrapeMax
-    case maxID of
-        Nothing  -> logError siteName "Failed retrieving maximum image ID"
-        Just max -> scrapeSiteFromTo site from max
+            let process ps = case filter (inRange (minID, maxID) . siteID) ps of
+                    [] -> io.log siteName $ "No new posts"
+                    ps -> do runXM $ mapM_ (xapianStore db) ps >> commit db
+                             mapM_ showPost ps
+                             go (n+1) db
 
-scrapeSiteFromTo :: SiteScraper -> Int -> Int -> IO ()
-scrapeSiteFromTo site@SiteScraper{..} from to = do
-    res <- runExceptT xapianRW
-    case res of
-        Left e   -> error e -- XXX: improve?
-        Right db -> forM_ [from..to] (go db)
-  where
-    go db id = do
-        res <- runExceptT $ processID db site id
-        case res of
-            Left e  -> logError siteName e
-            Right p -> putStrLn $ showPost p
+            case res of
+                Nothing -> throwE "Scraper failed to scrape page"
+                Just [] -> throwE "Scraper returned no results"
+                Just ps -> process ps
 
-    showPost Post{..} = "["++siteName++"] " ++ show siteID ++ "/" ++ show to
-                     ++ " " ++ fileURL ++ " " ++ unwords (map tagName tags)
 
--- Misc helpers
+          showPost Post{..} = io.log siteName $ unwords [ show siteID
+                                                        , fileURL
+                                                        , unwords tags ]
+
+-- ^ Misc helpers
 
 requireJust :: String -> IO (Maybe a) -> ExceptT String IO a
 requireJust err = maybe (throwE err) pure <=< liftIO
@@ -278,51 +238,20 @@ requireJust err = maybe (throwE err) pure <=< liftIO
 requireRight :: Show e => IO (Either e a) -> ExceptT String IO a
 requireRight = either (throwE . show) pure <=< liftIO
 
-logError :: String -> String -> IO ()
-logError name err = do
-    hPutStrLn stderr $ "["++name++"] Error: " ++ err
+log, logError :: String -> String -> IO ()
+log      name msg = hPutStrLn stdout $ "["++name++"] " ++ msg
+logError name err = hPutStrLn stderr $ "["++name++"] Error: " ++ err
 
--- Prefix mapping
+(<&>) :: Functor f => f a -> (a -> b) -> f b
+(<&>) = flip (<$>)
 
-tagPrefix :: TagType -> Char
-tagPrefix General   = 'G'
-tagPrefix Character = 'C'
-tagPrefix Artist    = 'A'
-tagPrefix Work      = 'W'
+io :: MonadIO m => IO a -> m a
+io = liftIO
 
-siteIDPrefix   = 'I'
-booruPrefix    = 'B'
-ratingPrefix   = 'R'
-uploaderPrefix = 'U'
-scorePrefix    = 'S'
-sourcePrefix   = 'R'
-urlPrefix      = 'L'
-
-prefixFriendly :: [(Char, [String])]
-prefixFriendly =
-    [ (siteIDPrefix,   ["id:"])
-    , (booruPrefix,    ["booru:", "scraper:", "site:"])
-    , (ratingPrefix,   ["rating:"])
-    , (uploaderPrefix, ["uploader:", "creator:"])
-    , (scorePrefix,    ["score:"])
-    , (sourcePrefix,   ["source:"])
-    , (urlPrefix,      ["url:"])
-
-    , (tagPrefix Character, ["character:", "char:"])
-    , (tagPrefix Artist,    ["artist:", "author:"])
-    , (tagPrefix Work,      ["work:", "copyright:"])
-
-    -- This must be the last entry
-    , (tagPrefix General, [""])
-    ]
-
-parseTerm :: String -> String
-parseTerm str = head [ p:r | (p, ls) <- prefixFriendly
-                           , l <- ls
-                           , r <- maybeToList $ chomp l str
-                     ]
-
--- TODO: thumbnail generation (?), frontend
+-- TODO: keep better track of what sites need to be scraped and from where
 
 -- XXX for testing
-main = scrapeSiteFromTo gelbooru 1280 10000
+main = do res <- runExceptT $ scrapeSite gelbooru 0 minBound maxBound
+          case res of
+            Left e  -> logError "global" e
+            Right _ -> return ()
